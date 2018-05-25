68fc58b9908dac76e35607fd772acda9a8522cb4
ksyro98 <ksyro98@protonmail.com>
2018-05-25 14:37:32 +0300

    Adding code.

--- app/src/main/java/com/example/dell/bakingtime/IngredientsWidgetProvider.java
+++ app/src/main/java/com/example/dell/bakingtime/IngredientsWidgetProvider.java
@@ -1,25 +1,32 @@
 package com.example.dell.bakingtime;
 
+import android.app.PendingIntent;
 import android.appwidget.AppWidgetManager;
 import android.appwidget.AppWidgetProvider;
 import android.content.Context;
+import android.content.Intent;
 import android.widget.RemoteViews;
 
-/**
- * Implementation of App Widget functionality.
- */
-public class IngredientsWidget extends AppWidgetProvider {
 
-    static void updateAppWidget(Context context, AppWidgetManager appWidgetManager,
-                                int appWidgetId) {
+public class IngredientsWidgetProvider extends AppWidgetProvider {
+
+    static void updateAppWidget(Context context, AppWidgetManager appWidgetManager, int appWidgetId) {
 
         CharSequence widgetText = context.getString(R.string.appwidget_text);
         // Construct the RemoteViews object
-        RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.ingredients_widget);
-        views.setTextViewText(R.id.appwidget_text, widgetText);
+        RemoteViews remoteViews = new RemoteViews(context.getPackageName(), R.layout.widget_list_view);
+
+        Intent intent = new Intent(context, MainActivity.class);
+        PendingIntent pendingIntent = PendingIntent.getActivity(context, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT);
+
+        remoteViews.setPendingIntentTemplate(R.id.list_view, pendingIntent);
+
+
+
+        //remoteViews.setTextViewText(R.id.appwidget_text, widgetText);
 
         // Instruct the widget manager to update the widget
-        appWidgetManager.updateAppWidget(appWidgetId, views);
+        appWidgetManager.updateAppWidget(appWidgetId, remoteViews);
     }
 
     @Override
